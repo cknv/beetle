65e37f5912de479a03bea44c61390ef305843388
Esben Sonne <esbensonne@gmail.com>
2014-07-12 19:28:50 +0200

    Template choice is now based on category.

--- beetle/renderers.py
+++ beetle/renderers.py
@@ -1,14 +1,22 @@
 from jinja2 import Environment, FileSystemLoader
+import os
 
 
 class TemplateRenderer:
     def __init__(self, template_folder):
         self.env = Environment(loader=FileSystemLoader(template_folder))
+        self.template_folder = template_folder
+        self.templates = {
+            name: template for name, template in self.load_templates()
+        }
+
+    def load_templates(self):
+        for template_file in os.listdir(self.template_folder):
+            name, extension = os.path.splitext(template_file)
+            yield name, self.env.get_template(template_file)
 
     def render_page(self, page, site):
-        template = self.env.get_template('template.html')
-        # print(page)
-        return template.render(page=page, site=site)
+        return self.templates[page['category']].render(page=page, site=site)
 
 
 def render_plain(raw_content):
