54de61beabb09ebd12189f82907c3970c51aa3ea
Esben Sonne <esbensonne@gmail.com>
2014-07-12 18:44:50 +0200

    Added dumb setup.py, needs more work (and better name).

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+import beetle
+
+setup(
+    name='beetle',
+    version=beetle.version,
+    author='Esben Sonne',
+    author_email='esbensonne@gmail.com',
+    url='https://github.com/cknv/beetle',
+    license='MIT',
+    keywords='',
+    packages=[
+        'beetle'
+    ],
+    entry_points={
+        'console_scripts': [
+            'beetle = beetle.cli:main',
+        ]
+    },
+    classifiers=[
+        '',
+    ],
+    install_requires=[
+        'PyYAML==3.11',
+        'awesome-slugify==1.5',
+    ],
+)
