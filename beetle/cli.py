0444bd79bd2a43cb5a8234aa2740eba1a087f3ac
Esben Sonne <esbensonne@gmail.com>
2014-07-12 19:39:17 +0200

    Dump of SimpleHTTPServer to serve html files.
    Taken from https://docs.python.org/2/library/simplehttpserver.html
    and prefixed with a hardcoded os.chdir('output') because I am lazy.

--- beetle/cli.py
+++ beetle/cli.py
@@ -1,9 +1,26 @@
 from .builder import Builder
 import yaml
+import sys
+import os
 
 
 def main():
-    with open('config.yaml') as f:
-        config = yaml.load(f.read())
-        builder = Builder(config)
-        builder.run()
+    if 'serve' in sys.argv:
+        os.chdir('output')
+        import SimpleHTTPServer
+        import SocketServer
+
+        PORT = 8000
+
+        Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+
+        httpd = SocketServer.TCPServer(("", PORT), Handler)
+
+        print("serving at port", PORT)
+        httpd.serve_forever()
+
+    else:
+        with open('config.yaml') as f:
+            config = yaml.load(f.read())
+            builder = Builder(config)
+            builder.run()
