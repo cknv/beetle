a55b2b6a39861e7b22baa1f50eae4104045a1855
Esben Sonne <esbensonne@gmail.com>
2014-10-13 19:39:13 +0200

    Cleaned up the utils and added a common reader.

--- beetle/utils.py
+++ beetle/utils.py
@@ -1,16 +1,9 @@
 import os
 
 
-def make_destination(file_name, new_extension=None):
-    __, destination = file_name.split(os.sep, 1)
-    if new_extension:
-        destination = change_extension(destination, new_extension)
-    return destination
-
-
-def change_extension(file_name, new_extension):
-    base, __ = os.path.splitext(file_name)
-    return '{base}.{extension}'.format(
-        base=base,
-        extension=new_extension,
-    )
+def read_folder(folder, mode):
+    for folder, __, files in os.walk(folder):
+        for file_name in files:
+            path = os.path.join(folder, file_name)
+            with open(path, mode) as fo:
+                yield path, fo.read()
